c0ab4cc58fe7d24ab4e9999bc5e27e0c7292e4a4
fasoria <71576596+fasoria@users.noreply.github.com>
2020-09-22 10:31:09 -0500

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -45,6 +45,16 @@
     console.log("Nuevo mensaje:"+message.payloadString); 
 	document.getElementById("sensor1").innerHTML=message.payloadString.split("=")[1];
 	document.getElementById("sensor2").innerHTML=message.payloadString.split("=")[2];
+	document.getElementById("sensor3").innerHTML=message.payloadString.split("=")[3];
+	document.getElementById("sensor4").innerHTML=message.payloadString.split("=")[4];
+	document.getElementById("sensor5").innerHTML=message.payloadString.split("=")[5];
+	
+	S1=document.getElementById("sensor")
+    S2=document.getElementById("sensor1")
+    S3=document.getElementById("sensor2")
+    S1.innerHTML=" "+Mensaje.split(":")[3];
+    S2.innerHTML=" "+Mensaje.split(":")[4];
+    S3.innerHTML=" "+Mensaje.split(":")[5];
     
   }
 
